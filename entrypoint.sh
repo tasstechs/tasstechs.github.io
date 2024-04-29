0599710f54fef7be09f7035057b75d952f7a7e6d
longtass <140433730+longtass@users.noreply.github.com>
2024-04-29 04:09:12 +0000

    check bundle

--- entrypoint.sh
+++ entrypoint.sh
@@ -69,7 +69,7 @@ if [[ -f "${GEMFILE_LOCK_DIR}/Gemfile.lock" ]]; then
     tail -n 1 | xargs)"
   echo "Bundler version ${BUNDLER_VER} is required by your Gemfile.lock!"
 fi
-
+bundle -v
 echo "Initial comptible bundler"
 ${SCRIPT_DIR}/script/cleanup_bundler.sh
 gem install bundler -v "${BUNDLER_VER}"
